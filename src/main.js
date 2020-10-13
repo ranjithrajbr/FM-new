c75e52f37a62d31b3616720f2bb1e0e85aa2792c
ranjith.raj <ranjith.raj@goideotic.com>
2020-10-13 12:49:28 +0530

    fixed styles

--- src/main.js
+++ src/main.js
@@ -21,18 +21,18 @@ Vue.component('ValidationProvider', ValidationProvider);
 
 
 import DefaultLayout from '~/layouts/Default.vue'
-import '~/assets/css/animate.css'
-import '~/assets/css/bootsnav.css'
-import '~/assets/css/bootstrap.min.css'
-import '~/assets/css/et-line-icons.css'
-import '~/assets/css/font-awesome.min.css'
-import '~/assets/css/justified-gallery.min.css'
-import '~/assets/css/magnific-popup.css'
-import '~/assets/css/modified.css'
-import '~/assets/css/responsive.css'
-import '~/assets/css/style.css'
-import '~/assets/css/test.css'
-import '~/assets/css/themify-icons.css'
+// import '~/assets/css/animate.css'
+// import '~/assets/css/bootsnav.css'
+// import '~/assets/css/bootstrap.min.css'
+// import '~/assets/css/et-line-icons.css'
+// import '~/assets/css/font-awesome.min.css'
+// import '~/assets/css/justified-gallery.min.css'
+// import '~/assets/css/magnific-popup.css'
+// import '~/assets/css/modified.css'
+
+// import '~/assets/css/style.css'
+// import '~/assets/css/test.css'
+// import '~/assets/css/themify-icons.css'
 
 
 
@@ -52,6 +52,21 @@ export default function (Vue, { router, head, isClient }) {
     name: 'description',
     content: 'First Main is an innovation scout and support aggregator that aims at building an entrepreneurial ecosystem, where every member mutually benefits one another.'
   })
+  head.link.push(
+ 
+  {rel:'stylesheet',href:'css/animate.css'},
+  {rel:'stylesheet',href:'css/bootsnav.css'},
+  {rel:'stylesheet',href:'css/bootstrap.min.css'},
+  {rel:'stylesheet',href:'css/et-line-icons.css'},
+  {rel:'stylesheet',href:'css/font-awesome.min.css'},
+  {rel:'stylesheet',href:'css/justified-gallery.min.css'},
+  {rel:'stylesheet',href:'css/magnific-popup.css'},
+  {rel:'stylesheet',href:'css/modified.css'},
+   {rel:'stylesheet',href:'css/responsive.css'},
+  {rel:'stylesheet',href:'css/style.css'},
+  {rel:'stylesheet',href:'css/test.css'},
+   {rel:'stylesheet',href:'css/themify-icons.css'},
+  )
 
 // head.script.push({
 //     src:'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js',
@@ -59,29 +74,29 @@ export default function (Vue, { router, head, isClient }) {
 //   })
 head.script.push(
 {src:'/js/jquery.js',body: true},
-{src:'js/jquery.easing.1.3.js',body: true,defer:true},
+{src:'js/jquery.easing.1.3.js',body: true},
 {src:'revolution/js/jquery.themepunch.tools.min.js',body: true, defer:true},
 {src:'revolution/js/jquery.themepunch.revolution.min.js',body: true, defer:true},
-{src:'js/modernizr.js',body: true},
-{src:'js/bootstrap.bundle.js',body: true},
-{src:'js/skrollr.min.js',body: true},
-{src:'js/smooth-scroll.js',body: true, defer:true},
-{src:'js/jquery.appear.js',body: true},
-{src:'js/bootsnav.js',body: true},
-{src:'js/jquery.nav.js',body: true},
-{src:'js/page-scroll.js',body: true},
-{src:'js/swiper.min.js',body: true},
-{src:'js/jquery.count-to.js',body: true},
-{src:'js/jquery.stellar.js',body: true},
-{src:'js/jquery.magnific-popup.min.js',body: true},
-{src:'js/isotope.pkgd.min.js',body: true},
-{src:'js/classie.js',body: true},
-{src:'js/hamburger-menu.js',body: true},
-{src:'js/counter.js',body: true},
-{src:'js/jquery.fitvids.js',body: true},
-{src:'js/skill.bars.jquery.js',body: true},
-{src:'js/justified-gallery.min.js',body: true},
-{src:'js/retina.min.js',body: true},
+{src:'js/modernizr.js',body: true, defer:true},
+{src:'js/bootstrap.bundle.js',body: true, defer:true},
+{src:'js/skrollr.min.js',body: true, defer:true},
+{src:'js/smooth-scroll.js',body: true, defer:true, defer:true},
+{src:'js/jquery.appear.js',body: true, defer:true},
+{src:'js/bootsnav.js',body: true, defer:true},
+{src:'js/jquery.nav.js',body: true, defer:true},
+{src:'js/page-scroll.js',body: true, defer:true},
+{src:'js/swiper.min.js',body: true, defer:true},
+{src:'js/jquery.count-to.js',body: true, defer:true},
+{src:'js/jquery.stellar.js',body: true, defer:true},
+{src:'js/jquery.magnific-popup.min.js',body: true, defer:true},
+{src:'js/isotope.pkgd.min.js',body: true, defer:true},
+{src:'js/classie.js',body: true, defer:true},
+{src:'js/hamburger-menu.js',body: true, defer:true},
+{src:'js/counter.js',body: true, defer:true},
+{src:'js/jquery.fitvids.js',body: true, defer:true},
+{src:'js/skill.bars.jquery.js',body: true, defer:true},
+{src:'js/justified-gallery.min.js',body: true, defer:true},
+{src:'js/retina.min.js',body: true, defer:true},
 
 {src:'js/main.js',body: true,defer:true}
 )
