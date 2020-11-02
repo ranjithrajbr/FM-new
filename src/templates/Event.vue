f6a63847cb511a7b75ea6ce27ec462a6efd623db
ranjith.raj <ranjith.raj@goideotic.com>
2020-11-02 16:03:11 +0530

    Added new

--- src/templates/Event.vue
+++ src/templates/Event.vue
@@ -7,7 +7,7 @@
                         <div class="col-12 col-lg-6 col-md-6 padding-20px-bottom">
                             <!-- <img :src="require(`../static/home-card/${image}`)" /> -->
                             <!-- <g-image :src="`home-card/${image}`"/> -->
-                          <g-image :src="`https://firstmain-backend.herokuapp.com${$page.event.image}`"/>
+                          <g-image :src="`https://res.cloudinary.com/fmcloud/image${$page.event.image}`"/>
                         </div>
                         <div class="col-12 col-lg-6 col-md-6">
                             <h3 class="text-uppercase">{{$page.event.title}}</h3>
